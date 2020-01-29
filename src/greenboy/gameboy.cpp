aba090055b0b7e853db60c69223a69e2a79b05b6
Claus Wiingreen <claus@wiingreen.dk>
2020-01-29 19:10:11 +0100

    added null checks for the Gameboy Class

--- src/greenboy/gameboy.cpp
+++ src/greenboy/gameboy.cpp
@@ -2,7 +2,14 @@
 
 namespace greenboy {
 Gameboy::Gameboy(std::unique_ptr<CPU> cpu, std::unique_ptr<Video> video)
-    : m_cpu{std::move(cpu)}, m_video(std::move(video)) {}
+    : m_cpu{std::move(cpu)}, m_video(std::move(video)) {
+  if (!m_cpu) {
+    throw std::invalid_argument("cpu cannot be null");
+  }
+  if (!m_video) {
+    throw std::invalid_argument("video cannot be null");
+  }
+}
 void Gameboy::step() {
   auto cycles = m_cpu->update();
   m_video->advance(cycles);
--- tests/greenboy/gameboy.cpp
+++ tests/greenboy/gameboy.cpp
@@ -16,12 +16,28 @@ public:
   MOCK_METHOD(void, advance, (cycles c), (override));
 };
 
+TEST(GameboyCtor, RejectsNullCPU) {
+  auto cpu = std::make_unique<MockCPU>();
+  EXPECT_THROW(Gameboy(std::move(cpu), nullptr), std::invalid_argument);
+}
+
+TEST(GameboyCtor, RejectsNullValues) {
+  auto video = std::make_unique<MockVideo>();
+  EXPECT_THROW(Gameboy(nullptr, std::move(video)), std::invalid_argument);
+}
+
+TEST(GameboyCtor, AcceptedConfiguration) {
+  auto cpu = std::make_unique<MockCPU>();
+  auto video = std::make_unique<MockVideo>();
+  EXPECT_NO_THROW(Gameboy(std::move(cpu), std::move(video)));
+}
+
 TEST(Step, CallsCPUUpdate) {
   auto cpu = std::make_unique<MockCPU>();
   auto video = std::make_unique<MockVideo>();
-  
+
   EXPECT_CALL(*cpu, update()).WillOnce(Return(cycles{1}));
-  
+
   Gameboy gameboy{std::move(cpu), std::move(video)};
 
   gameboy.step();
@@ -31,10 +47,12 @@ TEST(Step, CallsVideoAdvance) {
   auto cpu = std::make_unique<MockCPU>();
   auto video = std::make_unique<MockVideo>();
 
-  EXPECT_CALL(*cpu, update()).WillOnce(Return(cycles{1})).WillOnce(Return(cycles{2}));
+  EXPECT_CALL(*cpu, update())
+      .WillOnce(Return(cycles{1}))
+      .WillOnce(Return(cycles{2}));
   EXPECT_CALL(*video, advance(cycles{1}));
   EXPECT_CALL(*video, advance(cycles{2}));
-  
+
   Gameboy gameboy{std::move(cpu), std::move(video)};
 
   gameboy.step();
