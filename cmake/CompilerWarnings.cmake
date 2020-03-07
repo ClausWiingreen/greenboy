28ac6e05c083b3cb3c48d788bcb62389004cc809
Claus Wiingreen <claus@wiingreen.dk>
2020-03-07 22:38:09 +0100

    added warnings to the project

---
+++ cmake/CompilerWarnings.cmake
@@ -0,0 +1,67 @@
+function(set_warnings project)
+  option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" TRUE)
+
+  set(MSVC_WARNINGS 
+    /W4
+    /w14242
+    /w14254
+    /w14263
+    /w14265
+    /w14287
+    /w14289
+    /w14296
+    /w14311
+    /w14545
+    /w14546
+    /w14547
+    /w14549
+    /w14555
+    /w14619
+    /w14640
+    /w14826
+    /w14905
+    /w14906
+    /w14928
+  )
+  set(CLANG_WARNINGS 
+    -Wall 
+    -Wextra
+    -Wshadow
+    -Wnon-virtual-dtor
+    -Wold-style-cast
+    -Wcast-align
+    -Wunused
+    -Woverloaded-virtual
+    -Wpedantic
+    -Wconversion
+    -Wsign-conversion
+    -Wnull-dereference
+    -Wdouble-promotion
+    -Wformat=2
+  )
+  
+  if(WARNINGS_AS_ERRORS)
+    list(APPEND MSVC_WARNINGS /WX)
+    list(APPEND CLANG_WARNINGS -Werror)
+  endif()
+
+  set(GCC_WARNINGS 
+    ${CLANG_WARNINGS}
+    -Wmisleading-indentation
+    -Wduplicated-cond
+    -Wduplicated-branches
+    -Wlogical-op
+    -Wuseless-cast
+  )
+
+  if(MSVC)
+    set(PROJECT_WARNINGS ${MSVC_WARNINGS})
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    set(PROJECT_WARNINGS ${CLANG_WARNINGS})
+  else()
+    set(PROJECT_WARNINGS ${GCC_WARNINGS})
+  endif()
+
+  target_compile_options(${project} INTERFACE ${PROJECT_WARNINGS})
+endfunction()
+  
