b3362ae7aab10f235b825310c73d1cc3fd6ee9f3
Claus Wiingreen <claus@wiingreen.dk>
2020-03-11 08:46:17 +0100

    removed extra ;

--- src/greenboy/instruction.cpp
+++ src/greenboy/instruction.cpp
@@ -4,5 +4,5 @@ namespace greenboy::instructions {
 cycles NOP::execute(CPU::RegisterSet &registers, MemoryBus & /*memory*/) const {
   registers.pc++;
   return cycles{4};
-};
+}
 } // namespace greenboy::instructions
