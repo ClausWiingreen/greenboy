b98703888a57c85ea730b4830b210ddd588626ad
Claus Wiingreen <claus@wiingreen.dk>
2020-04-28 14:33:16 +0200

    fixed unsigned issue

--- include/greenboy/types.hpp
+++ include/greenboy/types.hpp
@@ -12,9 +12,9 @@ using std::to_integer;
 using word = std::uint16_t;
 
 constexpr word to_word(byte high, byte low) {
-  return static_cast<word>((to_integer<int>(high) << 8) | to_integer<int>(low));
+  return static_cast<word>((to_integer<unsigned>(high) << 8u) | to_integer<unsigned>(low));
 }
 
-constexpr byte high_byte(word w) { return static_cast<byte>(w >> 8); }
+constexpr byte high_byte(word w) { return static_cast<byte>(w >> 8u); }
 constexpr byte low_byte(word w) { return static_cast<byte>(w); }
 } // namespace greenboy
