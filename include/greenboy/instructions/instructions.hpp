a95b81d4eebcf234ca6b063e5861b77a142b07db
Claus Wiingreen <claus@wiingreen.dk>
2020-09-05 20:34:59 +0200

    Fixed an error with return value being hardcoded to return byte instead of the actual value type

--- include/greenboy/instructions/instructions.hpp
+++ include/greenboy/instructions/instructions.hpp
@@ -9,8 +9,7 @@
 
 namespace greenboy {
 
-template <typename T, typename U>
-struct enable_if_same : std::enable_if_t<std::is_same_v<T, U>> {};
+template <typename T, typename U> struct enable_if_same : std::enable_if_t<std::is_same_v<T, U>> {};
 
 template <typename T, typename = void> struct is_readable : std::false_type {};
 
@@ -18,8 +17,7 @@ template <typename T>
 struct is_readable<
     T, std::void_t<typename T::value_type,
                    enable_if_same<decltype(T::read),
-                                  typename T::value_type (*)(CPU::RegisterSet &,
-                                                             MemoryBus &)>>>
+                                  typename T::value_type (*)(CPU::RegisterSet &, MemoryBus &)>>>
     : std::true_type {};
 
 template <typename T, typename = void> struct is_writeable : std::false_type {};
@@ -27,9 +25,8 @@ template <typename T, typename = void> struct is_writeable : std::false_type {};
 template <typename T>
 struct is_writeable<
     T, std::void_t<typename T::value_type,
-                   enable_if_same<decltype(T::write),
-                                  void (*)(typename T::value_type,
-                                           CPU::RegisterSet &, MemoryBus &)>>>
+                   enable_if_same<decltype(T::write), void (*)(typename T::value_type,
+                                                               CPU::RegisterSet &, MemoryBus &)>>>
     : std::true_type {};
 
 template <typename T> constexpr bool is_readable_v = is_readable<T>::value;
@@ -67,10 +64,9 @@ template <Reg Name> struct Register {
     else if constexpr (Name == Reg::SP)
       return registers.sp;
   }
-  using value_type = decltype(
-      read(std::declval<CPU::RegisterSet &>(), std::declval<MemoryBus &>()));
-  static void write(value_type value, CPU::RegisterSet &registers,
-                    MemoryBus & /* memory */) {
+  using value_type =
+      decltype(read(std::declval<CPU::RegisterSet &>(), std::declval<MemoryBus &>()));
+  static void write(value_type value, CPU::RegisterSet &registers, MemoryBus & /* memory */) {
     if constexpr (Name == Reg::B)
       registers.b = value;
     else if constexpr (Name == Reg::C)
@@ -120,8 +116,8 @@ using SP = Register<Reg::SP>;
 } // namespace registers
 
 template <class ValueType = byte> struct Immediate {
-  using value_type = typename ValueType;
-  static byte read(CPU::RegisterSet &registers, MemoryBus &memory) {
+  using value_type = ValueType;
+  static value_type read(CPU::RegisterSet &registers, MemoryBus &memory) {
     if constexpr (std::is_same_v<value_type, word>) {
       auto low = memory.read(registers.pc++);
       auto high = memory.read(registers.pc++);
@@ -141,10 +137,10 @@ template <class ValueType = byte> struct Immediate {
 
 template <class Register, class ValueType = byte> struct Indirect {
   using value_type = ValueType;
-  static value_type read(CPU::RegisterSet &registers, MemoryBus &memory) {
-    static_assert(is_readable_v<Register>);
-    static_assert(std::is_same_v<typename Register::value_type, word>);
+  static_assert(is_readable_v<Register>);
+  static_assert(std::is_same_v<typename Register::value_type, word>);
 
+  static value_type read(CPU::RegisterSet &registers, MemoryBus &memory) {
     auto address = Register::read(registers, memory);
     if constexpr (std::is_same_v<value_type, word>) {
       auto low = memory.read(address);
@@ -154,11 +150,7 @@ template <class Register, class ValueType = byte> struct Indirect {
       return memory.read(address);
     }
   }
-  static void write(value_type value, CPU::RegisterSet &registers,
-                    MemoryBus &memory) {
-    static_assert(is_readable_v<Register>);
-    static_assert(std::is_same_v<typename Register::value_type, word>);
-
+  static void write(value_type value, CPU::RegisterSet &registers, MemoryBus &memory) {
     auto address = Register::read(registers, memory);
     if constexpr (std::is_same_v<value_type, word>) {
       memory.write(address, low_byte(value));
@@ -167,19 +159,17 @@ template <class Register, class ValueType = byte> struct Indirect {
       memory.write(address, value);
     }
   }
-  static constexpr cycles access_time() {
-    return cycles{4} + Register::access_time();
-  }
+  static constexpr cycles access_time() { return cycles{4} + Register::access_time(); }
 };
 
 template <class Data, class = typename Data::value_type> struct Increment;
 
 template <class Data> struct Increment<Data, word> {
   using value_type = word;
-  static word read(CPU::RegisterSet &registers, MemoryBus &memory) {
-    static_assert(is_readable_v<Data>);
-    static_assert(is_writeable_v<Data>);
+  static_assert(is_readable_v<Data>);
+  static_assert(is_writeable_v<Data>);
 
+  static word read(CPU::RegisterSet &registers, MemoryBus &memory) {
     auto value = Data::read(registers, memory);
     Data::write(value + 1, registers, memory);
     return value;
@@ -191,9 +181,10 @@ template <class Data, class = typename Data::value_type> struct Decrement;
 
 template <class Data> struct Decrement<Data, word> {
   using value_type = word;
+  static_assert(is_writeable_v<Data>);
+  static_assert(is_readable_v<Data>);
+
   static word read(CPU::RegisterSet &registers, MemoryBus &memory) {
-    static_assert(is_writeable_v<Data>);
-    static_assert(is_readable_v<Data>);
 
     auto value = Data::read(registers, memory);
     Data::write(value - 1, registers, memory);
@@ -204,30 +195,24 @@ template <class Data> struct Decrement<Data, word> {
 
 template <class High, class Low> struct Double {
   using value_type = word;
+  static_assert(is_readable_v<High>);
+  static_assert(is_readable_v<Low>);
+
   static word read(CPU::RegisterSet &registers, MemoryBus &memory) {
-    static_assert(is_readable_v<High>);
-    static_assert(is_readable_v<Low>);
     auto high = High::read(registers, memory);
     auto low = Low::read(registers, memory);
     return to_word(high, low);
   }
-  static void write(word value, CPU::RegisterSet &registers,
-                    MemoryBus &memory) {
-    static_assert(is_writeable_v<High>);
-    static_assert(is_writeable_v<Low>);
+  static void write(word value, CPU::RegisterSet &registers, MemoryBus &memory) {
     High::write(high_byte(value), registers, memory);
     Low::write(low_byte(value), registers, memory);
   }
-  static constexpr cycles access_time() {
-    return High::access_time() + Low::access_time();
-  }
+  static constexpr cycles access_time() { return High::access_time() + Low::access_time(); }
 };
 
 template <unsigned char Value> struct Constant {
   using value_type = byte;
-  static byte read(CPU::RegisterSet &registers, MemoryBus &memory) {
-    return byte{Value};
-  }
+  static byte read(CPU::RegisterSet &registers, MemoryBus &memory) { return byte{Value}; }
   static constexpr cycles access_time() { return cycles{0}; }
 };
 
@@ -246,17 +231,14 @@ template <class Register, class Offset> struct Offsat {
 
     return static_cast<word>(result);
   }
-  static constexpr cycles access_time() {
-    return Register::access_time() + Offset::access_time();
-  }
+  static constexpr cycles access_time() { return Register::access_time() + Offset::access_time(); }
 };
 
 template <typename Destination, typename Source>
 inline cycles load(CPU::RegisterSet &registers, MemoryBus &memory) {
   static_assert(is_readable_v<Source>);
   static_assert(is_writeable_v<Destination>);
-  static_assert(std::is_same_v<typename Source::value_type,
-                               typename Destination::value_type>);
+  static_assert(std::is_same_v<typename Source::value_type, typename Destination::value_type>);
 
   auto value = Source::read(registers, memory);
   Destination::write(value, registers, memory);
@@ -268,8 +250,7 @@ inline cycles add(CPU::RegisterSet &registers, MemoryBus &memory) {
   static_assert(is_readable_v<LHS>);
   static_assert(is_readable_v<RHS>);
   static_assert(is_writeable_v<LHS>);
-  static_assert(
-      std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
+  static_assert(std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
 
   if constexpr (std::is_same_v<typename LHS::value_type, word>) {
     auto lhs = LHS::read(registers, memory);
@@ -302,8 +283,7 @@ inline cycles add_with_carry(CPU::RegisterSet &registers, MemoryBus &memory) {
   static_assert(is_readable_v<LHS>);
   static_assert(is_readable_v<RHS>);
   static_assert(is_writeable_v<LHS>);
-  static_assert(
-      std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
+  static_assert(std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
 
   auto lhs = to_integer<int>(LHS::read(registers, memory));
   auto rhs = to_integer<int>(RHS::read(registers, memory));
@@ -324,8 +304,7 @@ inline cycles subtract(CPU::RegisterSet &registers, MemoryBus &memory) {
   static_assert(is_readable_v<LHS>);
   static_assert(is_readable_v<RHS>);
   static_assert(is_writeable_v<LHS>);
-  static_assert(
-      std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
+  static_assert(std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
 
   auto lhs = to_integer<int>(LHS::read(registers, memory));
   auto rhs = to_integer<int>(RHS::read(registers, memory));
@@ -342,13 +321,11 @@ inline cycles subtract(CPU::RegisterSet &registers, MemoryBus &memory) {
 }
 
 template <typename LHS, typename RHS>
-inline cycles subtract_with_carry(CPU::RegisterSet &registers,
-                                  MemoryBus &memory) {
+inline cycles subtract_with_carry(CPU::RegisterSet &registers, MemoryBus &memory) {
   static_assert(is_readable_v<LHS>);
   static_assert(is_readable_v<RHS>);
   static_assert(is_writeable_v<LHS>);
-  static_assert(
-      std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
+  static_assert(std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
 
   auto lhs = to_integer<int>(LHS::read(registers, memory));
   auto rhs = to_integer<int>(RHS::read(registers, memory));
@@ -369,8 +346,7 @@ inline cycles bitwise_and(CPU::RegisterSet &registers, MemoryBus &memory) {
   static_assert(is_readable_v<LHS>);
   static_assert(is_readable_v<RHS>);
   static_assert(is_writeable_v<LHS>);
-  static_assert(
-      std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
+  static_assert(std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
 
   auto lhs = to_integer<int>(LHS::read(registers, memory));
   auto rhs = to_integer<int>(RHS::read(registers, memory));
@@ -390,8 +366,7 @@ inline cycles bitwise_or(CPU::RegisterSet &registers, MemoryBus &memory) {
   static_assert(is_readable_v<LHS>);
   static_assert(is_readable_v<RHS>);
   static_assert(is_writeable_v<LHS>);
-  static_assert(
-      std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
+  static_assert(std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
 
   auto lhs = to_integer<int>(LHS::read(registers, memory));
   auto rhs = to_integer<int>(RHS::read(registers, memory));
@@ -411,8 +386,7 @@ inline cycles bitwise_xor(CPU::RegisterSet &registers, MemoryBus &memory) {
   static_assert(is_readable_v<LHS>);
   static_assert(is_readable_v<RHS>);
   static_assert(is_writeable_v<LHS>);
-  static_assert(
-      std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
+  static_assert(std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
 
   auto lhs = to_integer<int>(LHS::read(registers, memory));
   auto rhs = to_integer<int>(RHS::read(registers, memory));
@@ -432,8 +406,7 @@ inline cycles compare(CPU::RegisterSet &registers, MemoryBus &memory) {
   static_assert(is_readable_v<LHS>);
   static_assert(is_readable_v<RHS>);
   static_assert(is_writeable_v<LHS>);
-  static_assert(
-      std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
+  static_assert(std::is_same_v<typename LHS::value_type, typename RHS::value_type>);
 
   auto lhs = to_integer<int>(LHS::read(registers, memory));
   auto rhs = to_integer<int>(RHS::read(registers, memory));
@@ -473,8 +446,7 @@ cycles decrement(CPU::RegisterSet &registers, MemoryBus &memory) noexcept {
   }
 }
 
-template <typename Register>
-cycles push(CPU::RegisterSet &registers, MemoryBus &memory) noexcept {
+template <typename Register> cycles push(CPU::RegisterSet &registers, MemoryBus &memory) noexcept {
   static_assert(is_readable_v<Register>);
   static_assert(std::is_same_v<typename Register::value_type, word>);
 
@@ -486,8 +458,7 @@ cycles push(CPU::RegisterSet &registers, MemoryBus &memory) noexcept {
   return Register::access_time() + cycles{8};
 }
 
-template <typename Register>
-cycles pop(CPU::RegisterSet &registers, MemoryBus &memory) noexcept {
+template <typename Register> cycles pop(CPU::RegisterSet &registers, MemoryBus &memory) noexcept {
   static_assert(is_writeable_v<Register>);
   static_assert(std::is_same_v<typename Register::value_type, word>);
 
